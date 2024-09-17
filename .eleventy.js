13ed05fd97770ebefbb2b9c07f7d41407bfc209c
DisguisedAxolotl <jasperhalkowich@gmail.com>
2024-09-17 10:12:14 -0700

    Add nunjucks templating, more tests, add index.css

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 module.exports = function(eleventyConfig) {
     // Return your Object options:
+    eleventyConfig.addPassthroughCopy("views/index.css");
     return {
+      passthroughFileCopy: true,
       dir: {
         input: "views",
+        includes: "_includes",
+        data: "_data",
         output: "_site"
       }
     }
