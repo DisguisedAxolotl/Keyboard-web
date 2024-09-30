df187d8b51a3c7160c7272a1f63719d5e885df03
DisguisedAxolotl <jasperhalkowich@gmail.com>
2024-09-30 10:52:04 -0700

    add content to PCB page, main page

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,20 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("views/_includes/main.css");
     eleventyConfig.addPassthroughCopy("views/_data/glossary.json");
     eleventyConfig.addPassthroughCopy("views/_scripts/glossary.js");
+    // Collection for PCB posts
+    eleventyConfig.addCollection('pcb', function(collectionApi) {
+      return collectionApi.getFilteredByTag('pcb');
+    });
+
+    // Collection for Software posts
+    eleventyConfig.addCollection('software', function(collectionApi) {
+      return collectionApi.getFilteredByTag('software');
+    });
+
+    // Collection for Case posts
+    eleventyConfig.addCollection('case', function(collectionApi) {
+      return collectionApi.getFilteredByTag('case');
+    });
     return {
       passthroughFileCopy: true,
       dir: {
