028d7b2619f2e41e3959187d2fc99633bc40c0ef
DisguisedAxolotl <jasperhalkowich@gmail.com>
2024-10-02 10:26:49 -0700

    Change license, add content

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,41 @@
-module.exports = function(eleventyConfig) {
-    // Pass through the CSS file correctly
-    eleventyConfig.addPassthroughCopy("views/_includes/main.css");
-    eleventyConfig.addPassthroughCopy("views/_data/glossary.json");
-    eleventyConfig.addPassthroughCopy("views/_includes/glossary.js");
-    eleventyConfig.addPassthroughCopy("views/_includes/clipboard.js");
-    // Collection for PCB posts
-    eleventyConfig.addCollection('pcb', function(collectionApi) {
-      return collectionApi.getFilteredByTag('pcb');
-    });
+module.exports = async function(eleventyConfig) {
+  // Dynamically import the Pug plugin
+  const pugPlugin = await import("@11ty/eleventy-plugin-pug");
 
-    // Collection for Software posts
-    eleventyConfig.addCollection('software', function(collectionApi) {
-      return collectionApi.getFilteredByTag('software');
-    });
+  // Add the Pug plugin to Eleventy
+  eleventyConfig.addPlugin(pugPlugin.default);
 
-    // Collection for Case posts
-    eleventyConfig.addCollection('case', function(collectionApi) {
-      return collectionApi.getFilteredByTag('case');
-    });
-    return {
-      passthroughFileCopy: true,
-      dir: {
-        input: "views",
-        includes: "_includes",
-        data: "_data",
-        output: "_site"
-      }
-    }
+  // Pass through the CSS file correctly
+  eleventyConfig.addPassthroughCopy("views/_includes/main.css");
+  eleventyConfig.addPassthroughCopy("views/_data/glossary.json");
+  eleventyConfig.addPassthroughCopy("views/_includes/glossary.js");
+  eleventyConfig.addPassthroughCopy("views/_includes/clipboard.js");
+
+  // Collection for PCB posts
+  eleventyConfig.addCollection('pcb', function(collectionApi) {
+    return collectionApi.getFilteredByTag('pcb');
+  });
+
+  // Collection for Software posts
+  eleventyConfig.addCollection('software', function(collectionApi) {
+    return collectionApi.getFilteredByTag('software');
+  });
+
+  // Collection for Case posts
+  eleventyConfig.addCollection('case', function(collectionApi) {
+    return collectionApi.getFilteredByTag('case');
+  });
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "views",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    markdownTemplateEngine: "pug",
+    htmlTemplateEngine: "pug",
+    templateFormats: ["pug", "md"]
   };
-  
+};
--- README.md
+++
@@ -1,4 +0,0 @@
-# Keyboard-web
-A website to document my mechanical keyboard project.
-
-Hosted at [Keyboard-web.pages.dev](keyboard-web.pages.dev)
